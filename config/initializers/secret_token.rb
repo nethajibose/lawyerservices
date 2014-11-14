4e3a799c5ffb44cd16ce63e32d00ddaaaca239b0
nethaji <nethaji@railsfactory.org>
2014-11-14 23:16:33 +0530

    Initial commit for lawyer services

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lawyerservices::Application.config.secret_token = '086ed9db7ecc2bd2322ce2b8bc5ce47fcc809e130e0c1fc1b23810d4d8a85e388af5834fbe132680af8857bfd1425b5856f5d98833bb23abb60ee7db06008c23'
